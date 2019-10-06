e78dc368bbd8590daf9c4790fde90e9e41e10e07
FRANK CAMPO CASTILLA <fdccastilla@gmail.com>
2019-10-05 22:06:52 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB = `mongodb://ertuu:aa12345678@cluster0-shard-00-00-mvnwy.mongodb.net:27017,cluster0-shard-00-01-mvnwy.mongodb.net:27017,cluster0-shard-00-02-mvnwy.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
