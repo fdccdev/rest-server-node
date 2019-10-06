699152fb6553a61e1e9b6edd32f0bb07a67a972e
FRANK CAMPO CASTILLA <fdccastilla@gmail.com>
2019-10-05 23:06:55 -0500

    Config js adjust variables 2

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 let urlDB;
 
-//aa12345678
-
-//mongodb://ertuu:aa12345678@cluster0-shard-00-00-mvnwy.mongodb.net:27017,cluster0-shard-00-01-mvnwy.mongodb.net:27017,cluster0-shard-00-02-mvnwy.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
-
 if (process.env.NODE_ENV === "dev1") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
